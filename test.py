f7a66dbfe401174d53fb0937a236b15fd9f8610d
amaurihideki <amaurihideki@gmail.com>
2018-06-01 13:16:21 -0400

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps essentias for linux")
+        self.assertRegex(result.data.decode(), "DevOps essentias for linux.")
 
 
 if __name__ == "__main__":
